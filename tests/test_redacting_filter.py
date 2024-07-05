47fd9aed292475e8bb17dd6ba5d6be677b1480cc
Arman Jasuja <armanjasuja@dbz.local>
2024-07-05 11:17:03 +0400

    Added unit tests

--- tests/test_redacting_filter.py
+++ tests/test_redacting_filter.py
@@ -3,7 +3,6 @@ import pytest
 import logging
 import logredactor
 
-
 @pytest.fixture
 def logger_setup(request):
     def get_logger(filters):
@@ -12,7 +11,8 @@ def logger_setup(request):
         logger.addFilter(
             logredactor.RedactingFilter(
                 filters,
-                default_mask='****'
+                default_mask='****',
+                mask_keys={'phonenumber'}
             )
         )
         return logger
@@ -36,6 +36,12 @@ def test_arg_dict(caplog, logger_setup):
     logger = logger_setup([re.compile(r'\d{3}')])
     logger.warning("foo %(bar)s", {'bar': '123'})
     assert caplog.records[0].message == "foo ****"
+
+
+def test_arg_dict_with_key_to_remove(caplog, logger_setup):
+    logger = logger_setup([re.compile(r'\d{3}')])
+    logger.warning("foo %s", {'phonenumber': '123'})
+    assert caplog.records[0].message == "foo {'phonenumber': '****'}"
 
 
 def test_extra_string_value(caplog, logger_setup):
@@ -73,22 +79,7 @@ def test_extra_do_not_redact_key(caplog, logger_setup):
     assert caplog.records[0].thing987 == "foobar"
 
 
-def test_extra_nested_dict_with_list(caplog, logger_setup):
+def test_extra_do_redact_specific_key(caplog, logger_setup):
     logger = logger_setup([re.compile(r'\d{3}')])
-    extra_data = {
-        'bar': {
-            'thing': ['one', '456'],
-        },
-    }
-    logger.warning("foo", extra=extra_data)
-    assert caplog.records[0].bar['thing'][0] == 'one'
-    assert caplog.records[0].bar['thing'][1] == '****'
-
-
-def test_match_group(caplog, logger_setup):
-    # Nothing in the code has to change
-    # But this shows the use of a Positive Lookbehind
-    # https://www.regextutorial.org/positive-and-negative-lookbehind-assertions.php
-    logger = logger_setup([re.compile(r'(?<=api_key=)[\w-]+')])
-    logger.warning("example.com?api_key=this-is-my-key&sort=price")
-    assert caplog.records[0].message == "example.com?api_key=****&sort=price"
+    logger.warning("foo", extra={'phonenumber': 'foobar'})
+    assert caplog.records[0].phonenumber == "****"
