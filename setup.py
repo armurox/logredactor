0a8c1d9b69ae911692ea4f61ed8ed2c257b8133c
Arman Jasuja <armanjasuja@dbz.local>
2024-07-06 01:21:14 +0400

    Updated workflow logic to only release with github releases

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import os
 
 try:
     with open("README.md", "r") as f:
@@ -6,11 +7,17 @@ try:
 except FileNotFoundError:
     long_description = ""
 
+branch = os.getenv('GITHUB_REF', 'master').split('/')[-1]  # Default to 'master' if GITHUB_REF is not set
+url = f"https://github.com/armurox/loggingredactor/tree/{branch}"
+
+# Get version from environment variable
+version = os.getenv('RELEASE_VERSION', '0.0.1')  # Default to '0.0.1' if RELEASE_VERSION is not set
+
 setup(
     name="loggingredactor",
     packages=find_packages(),
-    version="0.0.1",
-    url="https://github.com/armurox/loggingredactor",
+    version=version,
+    url=url,
     description="Redact data in logs based on regex filters and keys",
     long_description=long_description,
     long_description_content_type="text/markdown",
