137765bf2a3114f3ce812df0c3b17848d983553c
Arman Jasuja <arman_jasuja@yahoo.com>
2024-07-06 17:01:53 +0400

    Update setup.py to automatically generate development status

--- setup.py
+++ setup.py
@@ -12,6 +12,14 @@ url = f"https://github.com/armurox/loggingredactor/tree/{branch}"
 
 # Get version from environment variable
 version = os.getenv('RELEASE_VERSION', '0.0.1')  # Default to '0.0.1' if RELEASE_VERSION is not set
+# Get development status
+split_version = version.split('-')
+if len(split_version) == 2:
+    release_mode = split_version[1][0].upper()
+    status = split_version[1][-1] + ' - ' + release_mode
+    status += 'lpha' if release_mode == 'A' else 'eta'
+else:
+    status = 'Production'
 
 setup(
     name="loggingredactor",
@@ -24,12 +32,12 @@ setup(
     author="Arman Jasuja",
     author_email="arman_jasuja@yahoo.com",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        f"Development Status :: {status}",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
