7037c495ff7de0b0849b27a7db61bd5422934c9d
Arman Jasuja <arman_jasuja@yahoo.com>
2024-07-06 21:19:14 +0400

    Update workflow to release with appropriate branch

--- get-branch-name.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Get the commit SHA from the tag
-TAG_COMMIT_SHA=$(git rev-list -n 1 $GITHUB_REF)
-
-# Fetch the branch name using the commit SHA
-BRANCH_NAME=$(git branch -r --contains $TAG_COMMIT_SHA | grep -v '\->' | grep -Eo 'origin/[^ ]+$' | grep -Eo '[^/]+$' | head -1)
-
-echo "branch=$BRANCH_NAME" >> $GITHUB_ENV
